0ec6452b025ec064eb2d3c518468adcfee096eb2
Patrick Wetherbee <pwetherb96@gmail.com>
2023-06-03 00:10:08 -0700

    initial text reading complete

--- background.js
+++ background.js
@@ -1,66 +1,109 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === "complete") {
-    chrome.scripting
-      .executeScript({
-        target: { tabId: tabId },
-        func: async () => {
-          console.log("getting job details div");
-          var jobDetailsDiv = document.getElementById("job-details");
-          if (jobDetailsDiv) {
-            const response = await chrome.runtime.sendMessage({
-              foundJobDescription: true,
-            });
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//   if (changeInfo.status === "complete") {
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//       chrome.scripting.executeScript({
+//         target: { tabId: tabs[0].id },
+//         func: function () {
+//           let elements = document.querySelectorAll(
+//             '[data-testid="tweetText"] span'
+//           );
+//           let texts = Array.from(elements).map(
+//             (element) => element.textContent
+//           );
+//           if (elements.length > 0) {
+//             chrome.runtime.sendMessage({
+//               executeFilter: true,
+//               elements,
+//               texts,
+//             });
+//           }
+//         },
+//       });
+//     });
+//   } else {
+//     chrome.action.setBadgeText({ text: "" });
+//   }
+// });
+
+function getTweets() {
+  const elements = document.querySelectorAll('[data-testid="tweetText"] span');
+  const texts = Array.from(elements).map((element) => element.textContent);
+  if (texts.length > 0) {
+    chrome.runtime.sendMessage({ executeFilter: true, texts });
+  }
+}
+
+function startObserving(tabId) {
+  chrome.scripting.executeScript({
+    target: { tabId },
+    func: () => {
+      // Create a new observer
+      const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+            // If new nodes are added, check for new tweets
+            const elements = document.querySelectorAll(
+              '[data-testid="tweetText"]'
+            );
+            const texts = Array.from(elements).map(
+              (element) => element.textContent
+            );
+            if (texts.length > 0) {
+              chrome.runtime.sendMessage({ executeFilter: true, texts });
+            }
           }
-        },
-      })
-      .then(() => {});
-  } else {
-    chrome.action.setBadgeText({ text: "" });
+        });
+      });
+
+      // Define the target node and config
+      const targetNode = document.getElementById("react-root");
+      const config = { childList: true, subtree: true };
+
+      // Start observing the target node
+      if (targetNode) {
+        observer.observe(targetNode, config);
+      }
+
+      // Save observer to window so it can be accessed later for cleanup
+      window.observer = observer;
+    },
+  });
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete" && tab.active) {
+    startObserving(tabId);
   }
 });
 
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+  chrome.scripting.executeScript({
+    target: { tabId },
+    func: () => {
+      // Stop observing when the tab is closed
+      if (window.observer) {
+        window.observer.disconnect();
+      }
+    },
+  });
+});
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.foundJobDescription) {
-    chrome.action.setBadgeBackgroundColor({ color: [255, 0, 0, 150] }, () => {
+  if (request.executeFilter) {
+    chrome.action.setBadgeBackgroundColor({ color: "#ddffdd" }, () => {
       console.log("call back text");
-      chrome.action.setBadgeText({ text: "1" });
+      console.log(request.texts);
+      chrome.action.setBadgeText({ text: request.texts.length.toString() });
     });
     console.log("executing script");
   }
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.executeAIGen) {
-    executeAIGen(request.jobDescription, sender).then(() => {
-      console.log("done");
+  if (request.executeFilter) {
+    // Store the texts in local storage
+    chrome.storage.local.set({ texts: request.texts }, function () {
+      console.log("Value is set to " + request.texts);
     });
   }
 });
-
-async function executeAIGen(jobDescription, sender) {
-  try {
-    const res = await fetch("http://localhost:3000/api/edit-default-resume", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        jobDescription,
-      }),
-    });
-    const data = await res.json();
-    console.log(data);
-    // execute script that sets the text area to the data
-    chrome.scripting.executeScript({
-      target: { tabId: sender.tab.id },
-      func: function (data) {
-        chrome.storage.local.set({ message: data.message });
-        document.getElementById("generated-resume-text").textContent =
-          data.message;
-      },
-      args: [data],
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
 import "./App.css";
-import { Box, Paper, ThemeProvider, Typography } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  Paper,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
-import { darkTheme } from "../styles/theme";
+import { darkTheme, lightTheme } from "../styles/theme";
+import { Stack } from "@mui/system";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const [foundTweets, setFoundTweets] = useState([]);
+
+  console.log("App");
+
+  // useEffect(() => {
+  //   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  //     if (request.action === "getSource") {
+  //       console.log(request.source); // This will log the source text sent from the content script
+  //     }
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    // Retrieve the texts from local storage
+    chrome.storage.local.get(["texts"], function (result) {
+      console.log("Value currently is " + result.texts);
+      setFoundTweets(result.texts);
+    });
+  }, []);
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <CssBaseline />
       <Box
         sx={{
@@ -14,6 +45,18 @@ function App() {
           bgcolor: "background.paper",
         }}
       >
+        <Stack
+          direction={"row"}
+          spacing={2}
+          justifyContent={"flex-end"}
+          sx={{
+            m: 1,
+          }}
+        >
+          <IconButton onClick={() => setDarkMode((prev) => !prev)}>
+            <LightModeIcon />
+          </IconButton>
+        </Stack>
         <Typography
           sx={{
             mt: 3,
@@ -26,6 +69,11 @@ function App() {
         <Typography variant="subtitle1" align="center">
           Content Filter GPT
         </Typography>
+        {foundTweets?.map((tweet) => (
+          <Paper sx={{ p: 2, m: 1 }}>
+            <Typography variant="body2">{tweet}</Typography>
+          </Paper>
+        ))}
       </Box>
     </ThemeProvider>
   );
